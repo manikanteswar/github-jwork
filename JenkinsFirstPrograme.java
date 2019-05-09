52690b34de2e43108755e164c55736650d6d3d99
Manikanteswar <manee.java@gmail.com>
2019-05-09 23:54:47 +0800

    java file changed

--- JenkinsFirstPrograme.java
+++ JenkinsFirstPrograme.java
@@ -3,7 +3,7 @@ public class JenkinsFirstPrograme {
 
 	public static void main(String[] args) {
 
-		System.out.println("JenkinsFirstPrograme main invoked");
+		System.out.println("JenkinsFirstPrograme main invoked..");
 	}
 
 }
